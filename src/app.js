d73d8044b3da28e8d9088445f8da55df25826937
Nandini Patel <nandini_patel96@hotmail.co.uk>
2019-09-13 12:20:57 +0100

    added game and main functions

--- src/app.js
+++ src/app.js
@@ -8,12 +8,19 @@ const rock_div = document.getElementById("r");
 const paper_div = document.getElementById("p");
 const scissors_div = document.getElementById("s");
 
-rock_div.addEventListener('click', function() {
-  console.log("You clicked on rock");
-});
-paper_div.addEventListener('click', function() {
-  console.log("You clicked on paper");
-});
-scissors_div.addEventListener('click', function() {
-  console.log("You clicked on scissors")
-});
+function game(userChoice) {
+  console.log(" nandini" + userChoice);
+}
+
+function main() {
+  rock_div.addEventListener('click', function() {
+    game("r");
+  });
+  paper_div.addEventListener('click', function() {
+    game("p");
+  });
+  scissors_div.addEventListener('click', function() {
+    game("s");
+  });
+};
+main();
