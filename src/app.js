77ca20923f60c76d01e855344c2e273f4e1c8d55
Nandini Patel <nandini_patel96@hotmail.co.uk>
2019-09-17 11:35:13 +0100

    add set timeout function to remove green glow after certain time

--- src/app.js
+++ src/app.js
@@ -28,7 +28,9 @@ function win(userChoice, computerChoice) {
   const smallCompWord = "comp".fontsize(3).sup();
   result_p.innerHTML = `${convertToWord(userChoice)}${smallUserWord} beats ${convertToWord(computerChoice)}${smallCompWord}. You win!`;
   document.getElementById(userChoice).classList.add('green-glow')
+  setTimeout(function() { document.getElementById(userChoice).classList.remove('green-glow') }, 300);
 };
+
 
 function lose(userChoice, computerChoice) {
   computerScore++;
